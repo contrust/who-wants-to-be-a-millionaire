a0ddd56d271677fd3df60ab681b1271ac45d49d0
artem <2001raf@gmail.com>
2022-06-16 23:10:29 +0500

    added api get friend call answer request

--- server.js
+++ server.js
@@ -5,12 +5,16 @@ const fs = require("fs");
 const bodyParser = require("body-parser");
 const session = require("express-session");
 const questionsFilePath = './static/questions/questions.json';
-const leaderboardPath = './static/leaderboard/leaderboard.json'
+const leaderboardPath = './static/leaderboard/leaderboard.json';
+const friendCallTemplatesPath = "./static/templates/callToFriendTemplates.txt";
 const rootDir = process.cwd();
 const port = 3000;
+const friendCallRightAnswerProbability = 0.5;
 
 let questionsData = require(questionsFilePath);
 let leaderboard = require(leaderboardPath);
+let friendCallTemplates = fs.readFileSync(friendCallTemplatesPath, "utf-8").split(/\r?\n/);
+let crutchDictionary = {"A": 0, "B": 1, "C": 2, "D": 3};
 
 
 const app = express();
@@ -27,14 +31,13 @@ app.engine(
 );
 
 
-
 app.use('/static', express.static('static'));
 app.use(session({
     resave: false,
     saveUninitialized: false,
     secret: "asdfunmmc",
 }));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.get("/", (_, res) => {
@@ -49,7 +52,7 @@ app.get("/start", (_, res) => {
 });
 
 app.post("/start", (req, res) => {
-    initializeNewGame(req);
+    refreshGameState(req);
     res.redirect("/game");
 });
 
@@ -60,7 +63,7 @@ app.get("/game", (req, res) => {
     });
 });
 
-app.get("/gameOver", (req, res) =>{
+app.get("/gameOver", (req, res) => {
     res.render("gameOver", {
         layout: "default",
         title: "Game Over",
@@ -77,14 +80,23 @@ app.get("/leaderboard", (req, res) => {
 });
 
 app.get("/api/getNextQuestion", (req, res) => {
-    res.json(getNextQuestion(req.session));
+    res.json(getNextQuestion(req));
 });
 
-app.get("/api/getCallFriendPrompt", (req, res) => {
-    //TODO
+app.get("/api/getFriendCallAnswer", (req, res) => {
+    if (req.session.friendCallAnswer) {
+        res.json();
+        return;
+    }
+    let template = friendCallTemplates[getRandomNonNegativeInteger(friendCallTemplates.length)];
+    console.log(friendCallTemplates.length, getRandomNonNegativeInteger(friendCallTemplates.length), friendCallTemplates, template);
+    let answer = getFriendCallAnswer(req.session.currentQuestion);
+    let friendCallAnswer = getFormattedFriendCallAnswer(template, answer);
+    req.session.friendCallAnswer = friendCallAnswer;
+    res.json({"friendCallAnswer": friendCallAnswer});
 });
 
-app.post("/api/sendScore", (req, res) =>{
+app.post("/api/sendScore", (req, res) => {
     req.session.score = req.body.score;
     updateLeaderboard(req.session.username, req.session.score);
     console.log(`${req.session.username}: ${req.session.score}`);
@@ -93,20 +105,19 @@ app.post("/api/sendScore", (req, res) =>{
 
 app.listen(port, () => console.log(`App listening on port ${port}`));
 
-function getNextQuestion(session){
-    const randomIdx = Math.floor(Math.random()*session.currentQuestionsSet.length)
-    const nextQuestion = session.currentQuestionsSet[randomIdx];
-    session.currentQuestionsSet.splice(randomIdx, 1);
+function getNextQuestion(req) {
+    const nextQuestionIndex = getRandomNonNegativeInteger(req.session.currentQuestionsSet.length);
+    const nextQuestion = req.session.currentQuestionsSet[nextQuestionIndex];
+    console.log(nextQuestion);
+    req.session.currentQuestionsSet.splice(nextQuestionIndex, 1);
+    req.session.currentQuestion = nextQuestion;
     return nextQuestion;
 }
 
-function updateLeaderboard(name, score){
-    if(name in leaderboard && leaderboard[name]>=score)
-        return;
-    if(!(name in leaderboard))
-        leaderboard[name] = {name: name, score: score};
-    else
-        leaderboard[name].score = score;
+function updateLeaderboard(name, score) {
+    if (name in leaderboard && leaderboard[name] >= score) return;
+    if (!(name in leaderboard)) leaderboard[name] = {name: name, score: score};
+    else leaderboard[name].score = score;
     fs.writeFile(leaderboardPath, JSON.stringify(leaderboard), (err) => {
         if (err) return console.log(err);
         console.log(JSON.stringify(leaderboard));
@@ -114,10 +125,28 @@ function updateLeaderboard(name, score){
     });
 }
 
-function initializeNewGame(req){
+function refreshGameState(req) {
     req.session.username = req.body.user;
     req.session.score = 0;
     req.session.currentQuestionsSet = questionsData['questions'];
-    req.session.currentQuestion = getNextQuestion(req.session);
+    req.session.currentQuestion = getNextQuestion(req);
+}
+
+function getRandomNonNegativeInteger(maxInteger) {
+    return Math.floor(Math.random() * maxInteger);
+}
+
+function getFormattedFriendCallAnswer(template, answer) {
+    return template.replace("*", answer);
+}
+
+function getFriendCallAnswer(questionData) {
+    if (Math.random() < friendCallRightAnswerProbability) return questionData["answer"];
+    else {
+        let answers = [...questionData["choices"]];
+        answers.splice(crutchDictionary[questionData["answer"]], 1);
+        console.log(answers);
+        return answers[getRandomNonNegativeInteger(answers.length)];
+    }
 }
 
